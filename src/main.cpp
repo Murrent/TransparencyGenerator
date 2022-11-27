2adf4183dd619984e8ebfb032ede70e379f043c6
Tempron <wickgrenm@gmail.com>
2022-11-27 02:50:39 +0100

    Functionality testing in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
+#include "AppManager.hpp"
 
 int main()
 {
+    AppManager appManager;
+    appManager.setFolder("C:/Users/Martin/Desktop/test");
+    if (!appManager.selectFile("test.png")){
+        std::cout << "File not found" << std::endl;
+        return 0;
+    }
+    appManager.setReplaceColor(sf::Color::White);
+    appManager.setReplaceColorWith(sf::Color::Transparent);
+    if (!appManager.setSaveFormat("png")) {
+        std::cout << "Invalid format" << std::endl;
+        return 0;
+    }
+    if (!appManager.saveFile("test2")) {
+        std::cout << "Save failed" << std::endl;
+        return 0;
+    }
+    std::cout << "Save successful" << std::endl;
     return 0;
 }
