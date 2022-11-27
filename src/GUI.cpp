b31fd2cc35ab78bae5f0760b0aadd9717df77b66
Tempron <wickgrenm@gmail.com>
2022-11-27 02:47:37 +0100

    Moved classes out of GUI, will finish later

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,1 +1,21 @@
 #include "GUI.hpp"
+
+GUI::GUI() {
+
+}
+
+void GUI::run() {
+
+}
+
+void GUI::init() {
+
+}
+
+void GUI::update() {
+
+}
+
+void GUI::draw() {
+
+}
--- src/GUI.hpp
+++ src/GUI.hpp
@@ -1,37 +1,13 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-
-class ImagePreview : public sf::Drawable {
-private:
-    sf::Texture texture;
-    sf::Sprite sprite;
-    sf::RectangleShape rectangleShape;
-    sf::Text text;
-
-    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
-};
-
-class Button : public sf::Drawable {
-private:
-    sf::RectangleShape rectangleShape;
-    sf::Text text;
-
-    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
-};
+#include "ui_components/Interactive.hpp"
 
 class GUI {
 private:
     sf::RenderWindow window;
     sf::Font font;
-    ImagePreview beforeImagePreview;
-    ImagePreview afterImagePreview;
-    Button replaceColorButton;
-    Button replaceColorWithButton;
-    Button saveButton;
-    Button pngButton, jpgButton, bmpButton, tgaButton;
-    std::vector<Button> folderButtons;
-    std::vector<Button> imageButtons;
+    std::vector<Interactive> interactives;
 
 public:
     GUI();
@@ -44,4 +20,3 @@ public:
 
     void draw();
 };
-
